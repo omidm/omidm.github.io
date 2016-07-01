5c01fd603453e103cd204c77e13e54e973680949
Omid Mashayekhi <omidm@stanford.edu>
2016-07-01 02:46:18 -0700

    added two functionalities: 1. navbar collapse hide after click. 2. scrole on link click, for the anchors.

---
+++ js/scroll.js
@@ -0,0 +1,21 @@
+
+$( document ).ready(function() {
+     
+  function scrollToAnchor(aid){
+        var aTag = $("h1[name='"+ aid +"']");
+        $('html,body').animate({scrollTop: aTag.offset().top - 50},'slow');
+  }
+  
+  $("#resume-link").click(function() {
+        scrollToAnchor('resume');
+  });
+
+  $("#projects-link").click(function() {
+        scrollToAnchor('projects');
+  });
+
+  $("#papers-link").click(function() {
+        scrollToAnchor('papers');
+  });
+
+});
